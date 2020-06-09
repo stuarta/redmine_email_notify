3bb1d2cf0d6bb12b401d18d218b0db23afa2f2cc
Stuart Auchterlonie <stuarta@mythtv.org>
2020-06-09 21:44:42 +0100

    Initial files

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_email_notify do
+  name 'Redmine Email Notify plugin'
+  author 'Stuart Auchterlonie'
+  description 'Email notification plugin for Redmine'
+  version '0.0.1'
+  url 'htts://github.com/stuarta/redmine_email_notify'
+  author_url 'https://github.com/stuarta'
+end
